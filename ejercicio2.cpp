214c5ec7951c8b2dfe48522aedeb62f06a8b8187
ciro <90120353+kixomg@users.noreply.github.com>
2026-05-12 10:12:38 -0300

    Renombrar variables: manu -> numeros

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,46 +1,21 @@
-#include<iostream>
-#include<vector>
-#include<algorithm> // Necesario para std::find o std::count
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-int main() {
-    vector<int> ferral; 
-    vector<int> joseo;
-    int manuel;
-
-    cout << "Ingrese numeros (0 para terminar):" << endl;
-
-    for(;;) {
-         
-		cin >> manuel;
-        
-        if (manuel == 0) break;
-
-     
-        if (count(ferral.begin(), ferral.end(), manuel) > 0) {
-            
-            
-            if (count(joseo.begin(), joseo.end(), manuel) == 0) {
-                joseo.push_back(manuel);
-            }
-        }
-
-        
-        ferral.push_back(manuel);
-    }
-
-   
-    cout << "Lista Completa" << endl;
-    for(int x : ferral) { cout << x << " "; }
-
-    cout << "\n\nValores Duplicados" << endl;
-    if(joseo.empty()) {
-        cout << "No hubo duplicados.";
-    } else {
-        for(int x : joseo) { cout << x << " "; }
-    }
-
-    cout << endl;
-    return 0;
+int main(){
+	
+	vector <int> numeros = {1, 2, 3, 4, 5, 6, 7, 8};
+	for(int i=0; i<8; i++){
+	cout<<numeros[i];	
+	}
+	
+	reverse(numeros.begin(), numeros.end());
+	cout<<"Vector invertido"<<endl;
+	
+	for(int j=0; j<8; j++){
+	cout<<numeros[j];	
+	}
+	
+	
+	return 0;
 }
