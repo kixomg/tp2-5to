ac4d44c245469650e606ee9c17f5978e10a59ef2
ciro <90120353+kixomg@users.noreply.github.com>
2026-05-12 10:09:32 -0300

    Renombrar variables: manu -> numeros

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 int main(){
 	
-	vector <int> manu = {1, 2, 3, 4, 5, 6, 7, 8};
+	vector <int> numeros = {1, 2, 3, 4, 5, 6, 7, 8};
 	for(int i=0; i<8; i++){
-	cout<<manu[i];	
+	cout<<numeros[i];	
 	}
 	
-	reverse(manu.begin(), manu.end());
+	reverse(numeros.begin(), numeros.end());
 	cout<<"Vector invertido"<<endl;
 	
 	for(int j=0; j<8; j++){
-	cout<<manu[j];	
+	cout<<numeros[j];	
 	}
 	
 	
