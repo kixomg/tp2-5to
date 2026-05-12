6d346c42c3c5c293a3f832e07183eab26f4833dd
ciro <90120353+kixomg@users.noreply.github.com>
2026-05-12 10:16:27 -0300

    Renombrar variables: v1 -> primerVector, v2 -> segundoVector

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm>   // sort, set_intersection
 using namespace std;
-int main(){
-	
-	vector <int> numeros = {1, 2, 3, 4, 5, 6, 7, 8};
-	for(int i=0; i<8; i++){
-	cout<<numeros[i];	
-	}
-	
-	reverse(numeros.begin(), numeros.end());
-	cout<<"Vector invertido"<<endl;
-	
-	for(int j=0; j<8; j++){
-	cout<<numeros[j];	
-	}
-	
-	
-	return 0;
+
+int main() {
+    // Inicialización de los dos vectores
+    vector<int> primerVector = {1, 3, 5, 7, 9, 11};
+    vector<int> segundoVector = {2, 3, 5, 8, 9, 12};
+
+    // Ordenar ambos vectores (requerido por set_intersection)
+    sort(primerVector.begin(), primerVector.end());
+    sort(segundoVector.begin(), segundoVector.end());
+
+    // Vector para almacenar la intersección
+    vector<int> interseccion;
+
+    // set_intersection escribe los elementos comunes en interseccion
+    set_intersection(
+        primerVector.begin(), primerVector.end(),
+        segundoVector.begin(), segundoVector.end(),
+        back_inserter(interseccion)
+    );
+
+    // Imprimir resultado
+    cout << "Intersección: ";
+    for (int x : interseccion)
+        cout << x << " ";
+    cout << endl;
+
+    return 0;
 }
